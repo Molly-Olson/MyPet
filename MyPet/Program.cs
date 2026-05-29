527d807c50b7c06ae90ee64777ce3cfb8511505e
Molly Olson <molly.olson@student.centralia.edu>
2026-05-28 20:46:36 -0700

    I missed something

--- MyPet/Program.cs
+++ MyPet/Program.cs
@@ -9,6 +9,10 @@
             Console.WriteLine("----------------------------");
             Console.WriteLine("Meet your new pet TIGER!");
             Console.WriteLine("----------------------------");
+
+            string relativePath = "Tiger.jpg";
+            string absolutePath = Path.GetFullPath(relativePath);
+            Console.WriteLine($"Image Path: {absolutePath}");
         }
     }
 }
---
+++ MyPet/TigerImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MyPet
+{
+    internal class TigerImage : Form
+    {
+        private PictureBox tigerPictureBox;
+        public TigerImage()
+        {
+            this.Text = "Your GLORIOUS pet Tiger!";
+            this.Size = new Size(400, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            tigerPictureBox = new PictureBox();
+            tigerPictureBox.Size = new Size(350, 350);
+            tigerPictureBox.Location = new Point(25, 25);
+            tigerPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+    }
+}
